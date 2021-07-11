e6a1b2612492f052faa19ea378557a8a91deacc8
DineshKumar-G <dinesh.k@access-designs.com>
2021-07-11 09:26:43 +0530

    proxy server

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
 });
 const path = require('path');
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000;
