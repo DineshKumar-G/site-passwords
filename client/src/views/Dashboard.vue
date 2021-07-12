606a677ad3bd04190ad8e73eae6527d7211da76c
DineshKumar-G <dinesh.k@access-designs.com>
2021-07-12 16:58:40 +0530

    gradient title

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -2,6 +2,27 @@
   <div class="window-height">
     <div class="q-pt-xl row q-gutter-md justify-center">
       <q-card
+        @click="$router.push({ path: '/websites' })"
+        class="my-card text-white cursor-pointer"
+        style="
+          background: linear-gradient(90deg, #f00b51 0%, #7366ff 100%);
+          min-width: 19rem;
+        "
+      >
+        <q-card-section>
+          <div class="text-h2 text-center text-bold text-capitalize">
+            {{ topSite.title }}
+          </div>
+          <div class="text-subtitle1 text-center text-bold">Top Website</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none text-center text-bold">
+          Number of Accounts {{ topSite.length }}
+        </q-card-section>
+      </q-card>
+    </div>
+    <div class="q-pa-md row q-gutter-md justify-center">
+      <q-card
         class="my-card text-white"
         style="background: linear-gradient(90deg, #f00b51 0%, #7366ff 100%)"
       >
@@ -31,27 +52,6 @@
             />
           </div>
           <div class="text-subtitle1 text-center text-bold">Total Accounts</div>
-        </q-card-section>
-      </q-card>
-    </div>
-    <div class="q-pa-md row q-gutter-md justify-center">
-      <q-card
-        @click="$router.push({ path: '/websites' })"
-        class="my-card text-white cursor-pointer"
-        style="
-          background: linear-gradient(90deg, #f00b51 0%, #7366ff 100%);
-          min-width: 19rem;
-        "
-      >
-        <q-card-section>
-          <div class="text-h2 text-center text-bold text-capitalize">
-            {{ topSite.title }}
-          </div>
-          <div class="text-subtitle1 text-center text-bold">Top Website</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none text-center text-bold">
-          Number of Accounts {{ topSite.length }}
         </q-card-section>
       </q-card>
     </div>
